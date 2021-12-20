54b4ec07983f7ab592eab5cbfa8c313e99f33f88
Vinicius Alencar <viniciusalencardev@gmail.com>
2021-12-20 00:02:22 -0300

    Otimizando a logica para a ditiacao automatica

--- script.js
+++ script.js
@@ -12,35 +12,32 @@ window.onload = () => {
 }
 
 async function animateDescription(element, words, duration) {
-    sleep(duration * 1000).then(() => {
-        animateWord(element, words[counter], 0)
-        counter = counter >= words.length - 1 ? 0 : counter + 1
-        animateDescription(element, words, duration)
-    })
+    animateWord(element, words[counter], 0)
+    await sleep(duration * 1000)
+    counter = counter >= words.length - 1 ? 0 : counter + 1
+    animateDescription(element, words, duration)
 }
 
 async function animateWord(element, word, letter = 0, growing = true) {
     if(growing) {
-        sleep(dutrationLetter).then(() => {
-            element.innerHTML = 'Desenvolvedor ' + word.slice(0, letter)
-            if(letter <= word.length)
-                animateWord(element, word, letter + 1)
-            else {
-                // console.log((duration * 1000) - (30 + (30 * word.length)))
-                sleep((duration - 2) * 1000).then(() => {
-                    animateWord(element, word, letter + 1, false)
-                })
-            }  
-        })
+        element.innerHTML = 'Desenvolvedor ' + word.slice(0, letter)
+        await sleep(dutrationLetter)
+        if(letter <= word.length)
+            animateWord(element, word, letter + 1)
+        else {
+            // let wait = (duration * 1000) - ((dutrationLetter * word.length) * 3)
+            // console.log(wait)
+            await sleep((duration - 2) * 1000)
+            animateWord(element, word, letter + 1, false)
+        }  
     } else {
-        sleep(dutrationLetter).then(() => {
-            word = word.slice(0, word.length - 1)
-            if(word.length > 0 || word != '') {
-                element.innerHTML = 'Desenvolvedor ' + word
-                animateWord(element, word, letter, false)
-            } else
-                element.innerHTML = 'Desenvolvedor '
-        })
+        word = word.slice(0, word.length - 1)
+        await sleep(dutrationLetter)
+        if(word.length > 0 || word != '') {
+            element.innerHTML = 'Desenvolvedor ' + word
+            animateWord(element, word, letter, false)
+        } else
+            element.innerHTML = 'Desenvolvedor '
     }
 }
 

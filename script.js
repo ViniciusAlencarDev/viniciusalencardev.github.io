c6984d7001a45f1bef7826ee54583bb9d579158c
Vinicius Alencar <viniciusalencardev@gmail.com>
2021-12-23 22:43:28 -0300

    Ajuste na estilização

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 let descriptionAnimate;
 let counter = 0;
 let duration = 3;
-let dutrationLetter = 80
+let durationLetter = 80
 
 window.onload = () => {
 
     descriptionAnimate = document.querySelector('#description-animate')  
     let words = ['Full Stack', 'ReactJS', 'NodeJS', 'PHP', 'NextJS']
 
-    animateDescription(descriptionAnimate, words, duration, 'Desenvolvedor ')
+    animateDescription(descriptionAnimate, words, duration, durationLetter, 'Desenvolvedor ')
 }
 
-async function animateDescription(element, words, duration, prefix = '') {
+async function animateDescription(element, words, duration, durationLetter, prefix = '') {
     while(true) {
         let newWord = "";
         for(i = 0; i < words[counter].length; i++) {
             newWord += words[counter][i];
             element.innerHTML = prefix + newWord
-            await sleep(100)
+            await sleep(durationLetter)
         }
         await sleep(duration * 1000)
         for(i = newWord.length; i >= 0; i--) {
             newWord = words[counter].slice(0, i);
             element.innerHTML = prefix + newWord
-            await sleep(100)
+            await sleep(durationLetter)
         }
         counter = counter >= words.length - 1 ? 0 : counter + 1
     }
@@ -39,18 +39,18 @@ async function animateDescription(element, words, duration, prefix = '') {
 // async function animateWord(element, word, letter = 0, growing = true) {
 //     if(growing) {
 //         element.innerHTML = 'Desenvolvedor ' + word.slice(0, letter)
-//         await sleep(dutrationLetter)
+//         await sleep(durationLetter)
 //         if(letter <= word.length)
 //             animateWord(element, word, letter + 1)
 //         else {
-//             // let wait = (duration * 1000) - ((dutrationLetter * word.length) * 3)
+//             // let wait = (duration * 1000) - ((durationLetter * word.length) * 3)
 //             // console.log(wait)
 //             await sleep((duration - 2) * 1000)
 //             animateWord(element, word, letter + 1, false)
 //         }  
 //     } else {
 //         word = word.slice(0, word.length - 1)
-//         await sleep(dutrationLetter)
+//         await sleep(durationLetter)
 //         if(word.length > 0 || word != '') {
 //             element.innerHTML = 'Desenvolvedor ' + word
 //             animateWord(element, word, letter, false)
